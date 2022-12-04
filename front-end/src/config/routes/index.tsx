771e82d5a189ba7500fbcc2d00e2c04bcc98a88a
pauloosouza <pauloo.bsb15@gmail.com>
2022-12-04 15:44:05 -0300

    ajustar fluxo de login

--- front-end/src/config/routes/index.tsx
+++ front-end/src/config/routes/index.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import { Dashboard } from '../../views/Dashboard';
 import { Links } from '../../views/Links';
 
@@ -6,6 +7,27 @@ import { Login } from '../../views/Login';
 import { Register } from '../../views/Register';
 
 export function AppRoutes() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    const itsAlreadyAuthenticated = ['/', '/register'];
+
+    if (token && itsAlreadyAuthenticated.includes(location.pathname)) {
+      navigate('/dashboard');
+    }
+  }, [token, location.pathname, navigate]);
+
+  useEffect(() => {
+    const userToken = sessionStorage.getItem('accessToken') || '';
+
+    if (!userToken) navigate('/');
+
+    setToken(userToken);
+  }, [navigate, token]);
+
   return (
     <Routes>
       <Route path="/" element={<Login />} />
